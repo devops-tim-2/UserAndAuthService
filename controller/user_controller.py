cdbb51d0648d456129b930202643938de6d7370b
Nenad Misic <nenad.misic997@gmail.com>
2021-06-17 23:02:13 +0200

    Add controller layer with all needed endpoints Closes #5

---
+++ controller/user_controller.py
@@ -0,0 +1,96 @@
+from flask_restful import Resource, reqparse
+
+dto_parser = reqparse.RequestParser()
+dto_parser.add_argument('username', type=str, help='Username for user account')
+dto_parser.add_argument('password', type=str, help='Password for user account')
+dto_parser.add_argument('role', type=str, help='Role for user account')
+dto_parser.add_argument('age', type=int, help='Age for user account')
+dto_parser.add_argument('sex', type=str, help='Sex for user account')
+dto_parser.add_argument('region', type=str, help='Region for user account')
+dto_parser.add_argument('interests', type=str, help='Interests for user account')
+dto_parser.add_argument('bio', type=str, help='Bio for user account')
+dto_parser.add_argument('website', type=str, help='Website for user account')
+dto_parser.add_argument('phone', type=str, help='Phone for user account')
+dto_parser.add_argument('mail', type=str, help='Email for user account')
+dto_parser.add_argument('profile_image_link', type=str, help='Profile image link for user account')
+dto_parser.add_argument('public', type=bool, help='Is profile public?')
+dto_parser.add_argument('taggable', type=bool, help='Is profile taggable?')
+
+login_parser = reqparse.RequestParser()
+login_parser.add_argument('username', type=str, help='Username for user account')
+login_parser.add_argument('password', type=str, help='Password for user account')
+
+follow_parser = reqparse.RequestParser()
+follow_parser.add_argument('src', type=str, help='Source of follow')
+follow_parser.add_argument('dst', type=str, help='Destination of follow')
+follow_parser.add_argument('mute', type=bool, help='Did the source mute the destination?')
+
+block_parser = reqparse.RequestParser()
+block_parser.add_argument('src', type=str, help='Source of follow')
+block_parser.add_argument('dst', type=str, help='Destination of follow')
+
+agent_request_parser = reqparse.RequestParser()
+agent_request_parser.add_argument('u_id', type=int, help='Agent entity id')
+
+class UserResource(Resource):
+    def __init__(self):
+        #self.service = service
+        pass
+
+    def get(self, user_id):
+        pass
+
+    def put(self, user_id):
+        pass
+
+    def delete(self, user_id):
+        pass
+
+class UserListResource(Resource):
+    def __init__(self):
+        #self.service = service
+        pass
+
+    def post(self):
+        pass
+
+class LoginResource(Resource):
+    def __init__(self):
+        #self.service = service
+        pass
+
+    def post(self):
+        pass
+
+class RegisterResource(Resource):
+    def __init__(self):
+        #self.service = service
+        pass
+
+    def post(self):
+        pass
+
+class FollowResource(Resource):
+    def __init__(self):
+        #self.service = service
+        pass
+
+    def post(self):
+        pass
+
+class MuteResource(Resource):
+    def __init__(self):
+        #self.service = service
+        pass
+
+    def get(self):
+        pass
+
+
+class BlockResource(Resource):
+    def __init__(self):
+        #self.service = service
+        pass
+
+    def post(self):
+        pass
