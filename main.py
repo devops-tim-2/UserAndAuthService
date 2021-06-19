e93e7f266da009cd3d2bd22719c41a2fe304366b
Nenad Misic <nenad.misic997@gmail.com>
2021-06-19 15:21:59 +0200

    Resolve sonarcloud security risks

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from common.config import setup_config
+from os import environ
 
 app, db = setup_config('dev')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=environ.get('FLASK_RUN_HOST'))
