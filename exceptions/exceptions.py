ebbe3265809e11dfe409b889816796f09bb7f953
nenad-misic <33166172+nenad-misic@users.noreply.github.com>
2021-06-20 17:13:02 +0200

    Add login functionality #7 (#37)

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -1,3 +1,8 @@
 class InvalidRoleException(Exception):
     def __init__(self, message="Invalid user role"):
         super().__init__(message)
+
+class InvalidCredentialsException(Exception):
+    def __init__(self, message="Invalid credentials"):
+        self.message = message
+        super().__init__(self.message)
